0a292f93dfd1581f0e1aff44b1e540c10228c650
bmalikay <baktybekovamalika29@gmail.com>
2025-02-09 13:13:24 +0600

    first git Malika

--- js/home_works.js
+++ js/home_works.js
@@ -1,20 +1,32 @@
-// MOVE BLOCK HW 1.2
+// проверка на валидность
+const emailInput = document.querySelector("#gmail_input");
+const emailButton = document.querySelector("#gmail_button");
+const emailResult = document.querySelector("#gmail_result");
 
-const parentBlock = document.querySelector('.parent_block');
-const childBlock = document.querySelector('.child_block');
+const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+emailButton.onclick = () =>{
+    if (gmailRegex.test(emailInput.value)) {
+        emailResult.innerHTML = "Валидный Gmail";
+        emailResult.style.color = "green";
+    } else {
+        emailResult.textContent = "Невалидный Gmail";
+        emailResult.style.color = "red";
+    }
+}
+// 2-e задание
+const parentBlock = document.querySelector(".parent_block");
+const childBlock = document.querySelector(".child_block");
 
-let positionX = 0
-let positionY = 0
+let position = 0;
+const speed = 5;
 
-const offWidth = parentBlock.offsetWidth - childBlock.offsetWidth
-const offHeight = parentBlock.offsetHeight - childBlock.offsetHeight
+function moveBlock() {
 
-const moveBlock = () => {
-    if (positionX < offWidth) positionX++
-    if (positionX >= offWidth && positionY < offHeight) positionY++
-    childBlock.style.left = `${positionX}px`
-    childBlock.style.top = `${positionY}px`
-    requestAnimationFrame(moveBlock)
+    if (position < parentBlock.offsetWidth - childBlock.offsetWidth) {
+        position += speed;
+        childBlock.style.left = `${position}px`;
+        requestAnimationFrame(moveBlock);
+    }
 }
 
 moveBlock();
