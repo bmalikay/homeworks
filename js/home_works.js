cb73761b9f68d2022f7dfe5cbbda02bcf2cca2a1
bmalikay <baktybekovamalika29@gmail.com>
2025-02-10 12:27:14 +0600

    hw_2

--- js/home_works.js
+++ js/home_works.js
@@ -14,19 +14,62 @@ emailButton.onclick = () =>{
     }
 }
 // 2-e задание
-const parentBlock = document.querySelector(".parent_block");
 const childBlock = document.querySelector(".child_block");
+const parentBlock = document.querySelector(".parent_block");
 
-let position = 0;
-const speed = 5;
+let positionX = 0;
+let positionY = 0;
+const offsetWidth = parentBlock.clientWidth - childBlock.clientWidth;
+const offsetHeight = parentBlock.clientHeight - childBlock.clientHeight;
 
-function moveBlock() {
-
-    if (position < parentBlock.offsetWidth - childBlock.offsetWidth) {
-        position += speed;
-        childBlock.style.left = `${position}px`;
+const moveBlock = () => {
+    if (positionX < offsetWidth && positionY === 0) {
+        positionX++;
+        childBlock.style.left = `${positionX}px`;
+        requestAnimationFrame(moveBlock);
+    } else if (positionX >= offsetWidth && positionY < offsetHeight) {
+        positionY++;
+        childBlock.style.top = `${positionY}px`;
+        requestAnimationFrame(moveBlock);
+    } else if (positionY >= offsetHeight && positionX > 0) {
+        positionX--;
+        childBlock.style.left = `${positionX}px`;
+        requestAnimationFrame(moveBlock);
+    } else if (positionX === 0 && positionY > 0) {
+        positionY--;
+        childBlock.style.top = `${positionY}px`;
         requestAnimationFrame(moveBlock);
     }
-}
-
+};
 moveBlock();
+
+// 2.2
+const secondsDisplay = document.getElementById("seconds");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
+
+let counter = 0;
+let timer = null;
+
+startButton.addEventListener("click", () => {
+    if (!timer) {
+        timer = setInterval(() => {
+            counter++;
+            secondsDisplay.textContent = counter;
+        }, 1000);
+    }
+});
+
+stopButton.addEventListener("click", () => {
+    clearInterval(timer);
+    timer = null;
+});
+
+resetButton.addEventListener("click", () => {
+    clearInterval(timer);
+    counter = 0;
+    secondsDisplay.textContent = counter;
+    timer = null;
+});
+
